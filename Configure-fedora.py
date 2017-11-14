55025e7c58131760ef06ebf99ea7857c1a28209c
Unknown <jake.maher.10@gmail.com>
2017-11-15 00:12:58 +1100

    Added functions
    Added funtions to allow for modular install process. Aiming to allow role based installation

--- Configure-fedora.py
+++ Configure-fedora.py
@@ -9,11 +9,16 @@ uid = os.getuid()
 if os.getuid() != 0:
     exit("You are not logged in as root. This script contains commands that must be run as root, please restart using sudo or run this script as root")
 
+
 def updates():
     os.system("dnf upgrade --refresh -y")
 
-def install-fedy():
+
+def installFedy():
     os.system("sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'")
 
-def system-tools():
-    os.system
+
+def systemTools():
+    os.system("dnf install -y samba-common eiciel zsh easytag gimp pavucontrol \
+    quadrapassel gnome-2048 trasnmission deja-dup tilix filezilla dconf-editor \
+    remmina")
