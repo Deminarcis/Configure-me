e997b8281bb3d2908f0780386636f270ec3b48b4
Unknown <jake.maher.10@gmail.com>
2017-11-15 22:47:26 +1100

    added function to change default shell

--- Configure-fedora.py
+++ Configure-fedora.py
@@ -7,7 +7,7 @@ user = os.getlogin()
 uid = os.getuid()
 
 if os.getuid() != 0:
-    exit("You are not logged in as root. This script contains commands that must be run as root, please restart using sudo or run this script as root")
+    exit("This script contains commands that must be run as root, please restart using sudo or run this script as root")
 
 
 def updates():
@@ -22,3 +22,9 @@ def systemTools():
     os.system("dnf install -y samba-common eiciel zsh easytag gimp pavucontrol \
     quadrapassel gnome-2048 trasnmission deja-dup tilix filezilla dconf-editor \
     remmina")
+
+
+def setZshForUser():
+    setZsh = raw_input("Would you like to set Zsh as your default shell?: ")
+    if setZsh == "Y" or "y":
+        os.system("chsh -s /usr/bin/zsh %s" % uid)
